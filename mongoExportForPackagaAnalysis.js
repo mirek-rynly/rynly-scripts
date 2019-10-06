8d4a22a65cf573ac3f83d83c8573f61347a23d2a
Mirek Kukla <mirekkukla@gmail.com>
2019-10-06 16:23:12 +0200

    all done

--- mongoExportForPackagaAnalysis.js
+++ mongoExportForPackagaAnalysis.js
@@ -1,6 +1,8 @@
 /* Mongo commands to export job / package info for spreadsheet / sql analysis*/
 /* global db */
 
+/////////////////////// package_to_job_mapping
+
 // for visualizing
 db.Jobs.aggregate([
   {$match: {JobId: 18}},
@@ -11,9 +13,64 @@ db.Jobs.aggregate([
 // actually create new collection
 db.Jobs.aggregate([
   {$unwind : "$Packages"},
-  {$project: {_id: 0, job_id: "$_id", job_number: "$JobId", package_db_id: "$Packages._id"}},
+  {$project: {_id: 0, job_db_id: "$_id", JobId: 1, package_db_id: "$Packages._id"}},
   {$out: "package_to_job_mapping"}
-]).pretty();
+]);
 
 // exporting to CSV from the command line
-// mongoexport --db rynly --collection package_to_job_mapping --type=csv --fields=job_id,job_number,package_db_id > rynly_job_analysis/package_to_job_mapping.csv
+// mongoexport --db rynlyproduction --collection package_to_job_mapping --type=csv --fields=job_db_id,JobId,package_db_id > prod_package_to_job_mapping.csv
+
+
+/////////////////////// packages.csv
+
+// visualizing
+db.Packages.find(
+  {},
+  {From: 0, To: 0, Items: 0, Sender: 0, Recipient: 0, Changes: 0}
+);
+
+// export to CSV from command line
+// mongoexport --db rynlyproduction --collection Packages --type=csv --fields=_id,TrackingNumber,DateCreated,AmountPaid,JobId,IsExpedited > prod_packages.csv
+
+
+/////////////////////// jobs.csv
+db.Jobs.find(
+  {},
+  {Packages: 0, HubLocation: 0, StartLocation: 0}
+).pretty();
+
+// export to CSV from command line
+// mongoexport --db rynlyproduction --collection Jobs --type=csv --fields=_id,JobId,Type,TotalDistance,TrafficTotalTime,TrafficTotalDistance,TotalTime,ActualTotalTime,PayAmount,Shippers,DateCreated,DateCompleted,ContainsExpeditedPackage, > prod_jobs.csv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
