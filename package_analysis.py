83caee1f4cdf7ce41e0f043f6e839ca24932e993
Mirek Kukla <mirekkukla@gmail.com>
2019-10-21 18:43:50 -0700

    remove shippers

--- package_analysis.py
+++ package_analysis.py
@@ -27,20 +27,24 @@ with open(os.path.join(ANALYSIS_FOLDER, "prod_package_to_job_mapping.csv")) as f
         package_to_job_ids[p_id].append(j_id)
         job_to_package_ids[j_id].append(p_id)
 
+# ignore shipper jobs for all sets unless otherwise noted
 job_by_id = {}
 shipper_job_ids = set()
-delivery_job_ids = set()
+nonshipper_delivery_job_ids = set()
 # _id,JobId,Type,TotalDistance,TrafficTotalTime,TrafficTotalDistance,TotalTime,ActualTotalTime,PayAmount,Shippers,DateCreated,DateCompleted,ContainsExpeditedPackage,
 with open(os.path.join(ANALYSIS_FOLDER, "prod_jobs.csv")) as f_j:
     for line in f_j.read().splitlines()[1:]:
         _id = line.split(",")[0]
-        job_by_id[_id] = line
-
-        if line.split(",")[2] == "1": # for Type, 0=pickup, 1=delivery
-            delivery_job_ids.add(_id)
 
         if "ShipperId" in line:
             shipper_job_ids.add(_id)
+            continue
+
+        job_by_id[_id] = line
+
+        if line.split(",")[2] == "1": # for Type, 0=pickup, 1=delivery
+            nonshipper_delivery_job_ids.add(_id)
+
 
 # SANITY CHECK: looks like two package records don't quite agree
 # if len(package_by_id) != len(package_to_job_ids):
@@ -55,24 +59,22 @@ with open(os.path.join(ANALYSIS_FOLDER, "prod_jobs.csv")) as f_j:
 #             print p_id
 
 num_packages = len(package_to_job_ids)
-num_total_jobs = len(job_by_id)
-num_delivery_jobs = len(delivery_job_ids)
+num_total_nonshipper_jobs = len(job_by_id)
+num_nonshipper_delivery_jobs = len(nonshipper_delivery_job_ids)
 print "Total packages: {}".format(len(package_to_job_ids))
-print "Total jobs: {} ({} delivery)".format(num_total_jobs, num_delivery_jobs)
-print "(of the above, {} are shipper jobs)".format(len(shipper_job_ids))
+print "Total non-shipper jobs: {} ({} delivery)".format(num_total_nonshipper_jobs, num_nonshipper_delivery_jobs)
+print "Todal shipper jobs: {}".format(len(shipper_job_ids))
 
 # JOB COST
 total_cost_no_shippers = 0
 delivery_cost_no_shippers = 0
 for _id, job_line in job_by_id.iteritems():
-    if _id in shipper_job_ids:
-        continue
 
     # _id,JobId,Type,TotalDistance,TrafficTotalTime,TrafficTotalDistance,TotalTime,ActualTotalTime,PayAmount,Shippers,DateCreated,DateCompleted,ContainsExpeditedPackage,
     cost = int(job_line.split(",")[8])
     total_cost_no_shippers += cost
 
-    if _id in delivery_job_ids:
+    if _id in nonshipper_delivery_job_ids:
         delivery_cost_no_shippers += cost
 
 print "Cost of all (non-shipper) jobs: total = {}, cost per package = {}".format(total_cost_no_shippers, 1.0 * total_cost_no_shippers / num_packages)
@@ -86,5 +88,5 @@ print "Cost of all jobs (including shippers): total = {}, cost per package = {}"
 print ""
 
 # ROUGH PACKAGES PER JOB
-print "Packages per pickup job: {}".format(1.0 * num_packages / (num_total_jobs - num_delivery_jobs))
-print "Packages per delivery job: {}".format(1.0 * num_packages / num_delivery_jobs)
+print "Packages per pickup job: {}".format(1.0 * num_packages / (num_total_nonshipper_jobs - num_nonshipper_delivery_jobs))
+print "Packages per delivery job: {}".format(1.0 * num_packages / num_nonshipper_delivery_jobs)
