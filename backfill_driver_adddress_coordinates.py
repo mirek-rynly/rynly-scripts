f45f2969f960f1f7ddc0e53bd655b97b6cf1db4f
Mirek Kukla <mirekkukla@gmail.com>
2019-11-13 07:59:14 -0800

    backfill action

--- backfill_driver_adddress_coordinates.py
+++ backfill_driver_adddress_coordinates.py
@@ -21,17 +21,35 @@ def main(environ=api.UAT):
     failed_lines = []
     fixed_lines = []
     all_lines = file_utils.get_lines("/Users/mirek/driver_addresses.csv")
+    # all_lines = file_utils.get_lines("/Users/mirek/temp_driver_address.csv")
+
     for address_str in all_lines[1:]: #skip header
         print address_str
 
         address_line = address_str.split(",")
-        recommended_address = get_recommended_address(environ, address_line)
-        if not recommended_address:
-            failed_lines.append(address_line)
-        else:
+        recommended_address = get_valid_or_recommended_address(environ, address_line)
+        if recommended_address:
             location = recommended_address["location"]
             fixed_line = address_line + [str(location["latitude"]), str(location["longitude"])]
             fixed_lines.append(fixed_line)
+        else: # try cleaning things up
+            [_id, first_name, last_name, email, address, city, state, zip_code, o_lat, o_lon] = address_line
+            clean_address = address
+            for splitter in [" apt", " #", " unit"]:
+                clean_address = clean_address.lower().split(splitter)[0]
+
+            clean_address_line = [_id, first_name, last_name, email, clean_address, city, state, zip_code, o_lat, o_lon]
+            recommended_address = get_valid_or_recommended_address(environ, clean_address_line)
+            if recommended_address:
+                print "\t\tFixed address '{}' -> '{}".format(address, clean_address)
+                location = recommended_address["location"]
+                fixed_line = address_line + [str(location["latitude"]), str(location["longitude"])]
+                fixed_lines.append(fixed_line)
+
+            else:
+                print "\t\tDidn't fix address '{}' -> '{}".format(address, clean_address)
+                failed_lines.append(address_line)
+
 
     print "exporting {} failed and {} fixed lines".format(len(failed_lines), len(fixed_lines))
 
@@ -48,8 +66,9 @@ def export_lines(lines, file_path, header_str):
             f.write(",".join(line) + "\n")
 
 
-def get_recommended_address(environ, address_line):
+def get_valid_or_recommended_address(environ, address_line):
     [_id, first_name, last_name, email, address, city, state, zip_code, o_lat, o_lon] = address_line
+
     address_payload = {
         "city": city,
         "line1": address,
@@ -68,8 +87,15 @@ def get_recommended_address(environ, address_line):
         print "\tErrors: {}".format(response["errors"])
         return None
 
-    data = raw_response.json()["data"]
-    return data["recommendedAddress"]
+    data = response["data"]
+
+    if data["validatedAddress"]:
+        return data["validatedAddress"]
+
+    if data["recommendedAddress"]:
+        return data["recommendedAddress"]
+
+    raise Exception("Bad state for {}".format(data))
 
 
 if __name__ == "__main__":
